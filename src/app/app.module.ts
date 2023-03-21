c364787af20d230f6c923bd77be08cc2199278a7
Kornel <kornel.berenyi@gmail.com>
2023-03-21 22:45:26 +0100

    Harmadik commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {JokeComponent} from "../joke/joke.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {NavbarComponent} from "../navbar/navbar.component";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JokeComponent
+    JokeComponent,
+    NavbarComponent
 
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
